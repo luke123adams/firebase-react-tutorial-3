502032d284b91998b65ab120f253799a367a8e11
luke123adams <lxa207@gmail.com>
2023-08-25 12:29:41 +0100

    fixed tailwind problems

--- firebase-auth-yt/src/App.js
+++ firebase-auth-yt/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Login from './components/Login'
+import Signup from './components/Signup';
+import Account from './components/Account'
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className='text-center text-3xl font-bold'>
+      Firebase Auth & Context
+    </h1>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/account' element={<Account />} />
+
+    </Routes>
     </div>
   );
 }
